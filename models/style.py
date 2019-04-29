2a25c1562466df4bf65ea9af675147fa8420ba13
Lauren Bell <laurengilbert0@gmail.com>
2019-04-29 11:52:36 +0100

    join table between styles and finds added

--- models/style.py
+++ models/style.py
@@ -2,7 +2,11 @@ from app import db, ma
 from marshmallow import fields
 from .find import Find, FindSchema
 
-
+finds_styles = db.Table(
+    'finds_styles',
+    db.Column('find_id', db.Integer, db.ForeignKey('finds.id'), primary_key=True),
+    db.Column('style_id', db.Integer, db.ForeignKey('styles.id'), primary_key=True)
+)
 
 class Style(db.Model):
 
@@ -13,14 +17,14 @@ class Style(db.Model):
     shop_logo_img_url = db.Column(db.String(180), nullable=False)
     price = db.Column(db.String(20), nullable=False)
     description = db.Column(db.String(1000), nullable=False)
-    finds = db.relationship('Find', backref='styles', uselist=True)
-    finds_id = db.Column(db.Integer, db.ForeignKey('finds.id'))
+    finds = db.relationship('Find', backref='styles', secondary='finds_styles', uselist=True)
+    find_id = db.Column(db.Integer, db.ForeignKey('finds.id'))
 
 
 
 class StyleSchema(ma.ModelSchema):
 
-    finds = fields.Nested('FindSchema', many=True, exclude='styles')
+    finds = fields.Nested('FindSchema', exclude='styles', many=True)
 
     class Meta:
         model = Style
